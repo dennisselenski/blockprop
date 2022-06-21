490a0c76cb6fce85caca2e59e6e50592a1fd4091
Dennis <32433744+dennisselenski@users.noreply.github.com>
2022-06-21 16:13:30 +0200

    Update BlockProp.sol

--- BlockProp.sol
+++ BlockProp.sol
@@ -2,7 +2,20 @@
 pragma solidity >=0.4.0 
 
 contract BlockProp {
-
+          
+          srtruct Stakeholder {
+              int taxID;
+              Property[] propertyList;
+              
+              }
+              
+              
+           struct Property {
+           
+                //geo Data
+                
+           
+           }
 
 
 }
