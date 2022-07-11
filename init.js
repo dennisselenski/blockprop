0c78a0d6df7775de75bbaa6c1c8201667e6c6d8f
l.thyssen <l.thyssen@roometric.com>
2022-07-11 17:05:20 +0200

    merge status/init

--- init.js
+++ init.js
@@ -1,6 +1,8 @@
 // Contracts
 const Blockprop = artifacts.require("Blockprop");
-
+blockList = []
+ownerList = []
+ownerAdressList = []
 // Utils
 // const ether = (n) => {
 //   return new web3.utils.BN(
@@ -32,10 +34,49 @@ module.exports = async function(callback) {
     console.log('User 1:', u1)
     console.log('User 2:', u2)
     //todo call makeoffer from other addresses
+
+
+    
+    blockCount = await instance.getNumberOfBlocks.call()
+
+        for (i = 0; i < blockCount; i++) {
+            blockId = await instance.blocksList.call(i)
+            blockId = Number(blockId)
+
+            block = await instance.blocks.call(blockId)
+
+            var blockObj = new Object()
+            blockObj.x = BigInt(block.x)
+            blockObj.y = BigInt(block.y)
+            blockObj.size = BigInt(block.size)
+            blockObj.owner = block.owner
+            blockObj.propertyID = BigInt(block.propertyID)
+            blockObj.requester = block.requester
+            blockObj.offeredAmount = BigInt(block.offeredAmount)
+            blockList.push(blockObj)
+
+            if (!ownerAdressList.includes(blockObj.owner)) {
+                ownerAdressList.push(blockObj.owner)
+
+                owner = await instance.owners.call(blockObj.owner)
+
+                var ownerObj = new Object()
+                ownerObj.name = owner.name
+                ownerObj.taxID = owner.taxID
+                ownerObj.etherID = owner.etherID
+                ownerObj.authority = owner.authority
+                ownerList.push(ownerObj)
+            }
+        }
   }
   catch(error) {
     console.log(error)
   }
-
+  
+  var status = new Object()
+  status.blockList = blockList
+  status.ownerList = ownerList
+  
+  console.log(status)
   callback()
 }
